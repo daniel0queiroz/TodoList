51e483e0abdf8b96ac0f0e176662edee8ef9a1f1
Daniel Queiroz <daniel.olivqueiroz@outlook.com>
2022-10-18 21:32:30 -0300

    lista de tarefas - casamento

---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <main>
+    <h1>Lista de Tarefas</h1>
+    <p>Lista de afazeres para o casamento</p>
+
+    <div class="create-new">
+      <input 
+      type="text" 
+      v-model="newTask" placeholder="Adicionar uma nova tarefa" 
+      @keypress.enter="addTask" />
+      <button @click="addTask">Add</button>
+    </div>
+
+    <div class="tasks">
+      <Task
+      v-for="(task, i) in $store.state.tasks" :key="i"
+      :task="task" />
+    </div>
+
+  </main>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      newTask: ''
+    }
+  },
+  methods: {
+    addTask () {
+      if (this.newTask) {
+        this.$store.commit('ADD_TASK', this.newTask);
+        this.newTask = '';
+      }
+    }
+  }
+}
+</script>
